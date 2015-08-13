3bb06c1348ff5e9ddcf53fffe042d26600782ebb
briask <briask247@gmail.com>
2015-08-13 11:24:52 +0100

    Hello World web api

---
+++ MatchEvents/Controllers/MatchEventController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MatchEvents.Controllers
+{
+    public class MatchEventController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
